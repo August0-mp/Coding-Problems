fc8628c7a20dbfa2ab2ad4902dc6815b3a136563
August0-mp <augusto.procode@gmail.com>
2025-10-20 22:22:09 +0200

    .

---
+++ HackerRank/JumpingOnCLouds.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'jumpingOnClouds' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts INTEGER_ARRAY c as parameter.
+#
+
+def jumpingOnClouds(c):
+    # Write your code here
+    jumps = 0
+    i = 0
+    l =len(c)
+    while i < l-2:
+        if c[i+2] == 1:
+            i-=1
+        i+=2
+        jumps+=1
+        
+    return jumps if i == l-1 else jumps+1
+        
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    c = list(map(int, input().rstrip().split()))
+
+    result = jumpingOnClouds(c)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
